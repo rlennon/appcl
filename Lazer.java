40d636ca158ac6567b56af814d1946791fa962dc
shannondolan <l00128061@student.lyit.ie>
2018-11-06 19:51:42 +0000

    Created Lazer class

---
+++ Lazer.java
@@ -0,0 +1,15 @@
+public class Lazer
+{
+   public boolean isOperational;
+   public int weight;
+   
+   public Lazer(int weightIn)
+   {
+      this.weight = weightIn;
+   }
+   
+   public void isOperated()
+   {
+      isOperational = true;
+   }
+}
