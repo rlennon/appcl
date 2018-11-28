74afafd783117dea085013dfe81347fbe2b024f3
cjmcgowanlyit <L00118857@student.lyit.ie>
2018-11-28 23:33:47 +0000

    Completed Junit test

--- ScarletOverkill/src/dev/DestinationClassTest.java
+++ ScarletOverkill/src/dev/DestinationClassTest.java
@@ -2,27 +2,38 @@ package dev;
 
 import static org.junit.Assert.*;
 
-
 import org.junit.Test;
+
 
 public class DestinationClassTest {
 
 	@Test
 	public void testSetNumPlanes() {
 		//fail("Not yet implemented");
-		int numPlanesIn = 7;
+		//Create new DestinationClass object called test
+		DestinationClass test = new DestinationClass();
+		//set number of planes to 2
+		test.setNumPlanes(2);
+		//check that the number of planes does equal 2
+		assertTrue(test.getNumPlanes() == 2);
 	}
 
 	@Test
 	public void testGetNumPlanes() {
 		//fail("Not yet implemented");
-		//int j = this.testGetNumPlanes();
-		//return j;
+		//Create new DestinationClass object called test
+		DestinationClass test = new DestinationClass();
+		//set number of planes to 3
+		test.setNumPlanes(3);
+		//check that the number of planes does equal 3
+		assertTrue(test.getNumPlanes() == 3);
+		
 	}
 
 	@Test
+	//leave this test blank to show example failure message
 	public void testAtCapacity() {
-		//fail("Not yet implemented");
+		fail("Not yet implemented");
 	}
 
 }
