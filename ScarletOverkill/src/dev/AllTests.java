4805325aa8ea1481ab8ee17e6393ad021f1dbb93
Stephen Devenney <L00117389@student.lyit.ie>
2018-11-28 23:11:46 +0000

    Test classes for code coverage report

---
+++ ScarletOverkill/src/dev/AllTests.java
@@ -0,0 +1,11 @@
+package dev;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DestinationClassTest.class, RocketTest.class })
+public class AllTests {
+
+}
